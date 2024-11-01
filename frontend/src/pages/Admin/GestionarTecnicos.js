093a5088420e9308cf0c7e8284d916e764dbd027
Juan <juanda19-2001@hotmail.com>
2024-11-01 12:27:48 -0500

    Arreglos css y añadido gestionartecnios

--- frontend/src/pages/Admin/GestionarTecnicos.js
+++ frontend/src/pages/Admin/GestionarTecnicos.js
@@ -1,129 +1,256 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Alert from "../Alert/Alert.js";
 import "./GestionarTecnicos.css";
+import adminBG from "../../assets/images/adminBG.jpg";
+import Alert from "../Alert/Alert";
+import showIcon from "../../assets/images/show-icon.png";
+import hideIcon from "../../assets/images/hide-icon.png";
 
 const GestionarTecnicos = () => {
-  const [tecnicos, setTecnicos] = useState([]);
-  const [formData, setFormData] = useState({
-    nombre: "",
-    password: "",
-    ciudad: "",
-  });
-  const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [tecnicos, setTecnicos] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [ciudad, setCiudad] = useState("");
+  const [password, setPassword] = useState("");
+  const [tecnicoSeleccionado, setTecnicoSeleccionado] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const fetchTecnicos = async () => {
+    const token = localStorage.getItem("token");
+
     try {
-      const response = await fetch("http://localhost:4000/api/tecnicos");
-      const data = await response.json();
-      setTecnicos(data);
+      const response = await fetch("http://localhost:4000/api/tecnicos", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setTecnicos(data);
+      } else {
+        setAlertMessage("Error al obtener los datos de técnicos.");
+        setShowAlert(true);
+      }
     } catch (error) {
-      setError("Error al obtener técnicos.");
-      console.error("Error al obtener técnicos:", error);
+      setAlertMessage("Error del servidor. Intenta más tarde.");
+      setShowAlert(true);
     }
   };
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+
     fetchTecnicos();
-  }, []);
+  }, [navigate]);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const handleAgregarTecnico = async () => {
+    if (isEditMode) return;
+    setAlertMessage("");
+    setShowAlert(false);
 
-  const handleAdd = async (e) => {
-    e.preventDefault();
     try {
+      const token = localStorage.getItem("token");
       const response = await fetch("http://localhost:4000/api/tecnicos", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+        body: JSON.stringify({ nombre, ciudad, password }),
       });
-      if (!response.ok) {
+
+      if (response.ok) {
+        setNombre("");
+        setCiudad("");
+        setPassword("");
+        fetchTecnicos();
+        setAlertMessage("Técnico agregado correctamente");
+        setShowAlert(true);
+      } else {
         const errorData = await response.json();
-        setError(errorData.message || "Error desconocido al agregar técnico.");
-        return;
+        setAlertMessage(errorData.message || "Error al agregar técnico");
+        setShowAlert(true);
       }
-      fetchTecnicos();
-      setFormData({ nombre: "", password: "", ciudad: "" });
-      setError("");
     } catch (error) {
-      setError("Error en el servidor al agregar técnico.");
-      console.error("Error al agregar técnico:", error);
+      setAlertMessage("Error al agregar técnico");
+      setShowAlert(true);
     }
   };
 
-  const handleDelete = async (id) => {
+  const handleActualizarTecnico = async () => {
     try {
-      const response = await fetch(`http://localhost:4000/api/tecnicos/${id}`, {
-        method: "DELETE",
-      });
-      if (!response.ok) {
-        setError("Error al eliminar técnico.");
-        return;
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `http://localhost:4000/api/tecnicos/${tecnicoSeleccionado}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+          body: JSON.stringify({ nombre, ciudad, password }),
+        }
+      );
+
+      if (response.ok) {
+        fetchTecnicos();
+        setNombre("");
+        setCiudad("");
+        setPassword("");
+        setTecnicoSeleccionado("");
+        setAlertMessage("Técnico actualizado correctamente");
+        setShowAlert(true);
+      } else {
+        const errorData = await response.json();
+        setAlertMessage(errorData.message || "Error al actualizar técnico");
+        setShowAlert(true);
       }
-      fetchTecnicos();
-      setError("");
     } catch (error) {
-      setError("Error en el servidor al eliminar técnico.");
-      console.error("Error al eliminar técnico:", error);
+      setAlertMessage("Error al actualizar técnico");
+      setShowAlert(true);
     }
+  };
+
+  const handleEliminarTecnico = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        `http://localhost:4000/api/tecnicos/${tecnicoSeleccionado}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+        }
+      );
+
+      if (response.ok) {
+        setTecnicoSeleccionado("");
+        setNombre("");
+        setCiudad("");
+        setPassword("");
+        fetchTecnicos();
+        setAlertMessage("Técnico eliminado correctamente");
+        setShowAlert(true);
+      } else {
+        const errorData = await response.json();
+        setAlertMessage(errorData.message || "Error al eliminar técnico");
+        setShowAlert(true);
+      }
+    } catch (error) {
+      setAlertMessage("Error al eliminar técnico");
+      setShowAlert(true);
+    }
+  };
+
+  const handleTecnicoChange = (id) => {
+    const tecnico = tecnicos.find((t) => t.id_tecnico === id);
+    setTecnicoSeleccionado(id);
+    setNombre(tecnico ? tecnico.nombre : "");
+    setCiudad(tecnico ? tecnico.ciudad : "");
+    setPassword("");
+    setIsEditMode(!!id);
   };
 
   return (
-    <div className="gestionar-tecnicos">
-      <h2>Gestionar Técnicos</h2>
-      {error && <Alert message={error} onClose={() => setError("")} />}
-      <form onSubmit={handleAdd}>
+    <div
+      className="gestionar-tecnicos"
+      style={{ backgroundImage: `url(${adminBG})`, backgroundSize: "cover" }}
+    >
+      <div className="title-tecnico-container">
+        <h1 className="gestionar-titulo">Gestionar Técnicos</h1>
+      </div>
+      <div className="tecnico-form-container">
+        <select
+          className="tecnico-select-admin full-width"
+          value={tecnicoSeleccionado}
+          onChange={(e) => handleTecnicoChange(e.target.value)}
+        >
+          <option value="">Seleccione un técnico</option>
+          {tecnicos.map((tecnico) => (
+            <option key={tecnico.id_tecnico} value={tecnico.id_tecnico}>
+              {tecnico.id_tecnico} - {tecnico.nombre}
+            </option>
+          ))}
+        </select>
         <input
           type="text"
-          name="nombre"
           placeholder="Nombre"
-          value={formData.nombre}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Contraseña"
-          value={formData.password}
-          onChange={handleChange}
-          required
+          className="tecnico-input"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
         />
         <input
           type="text"
-          name="ciudad"
           placeholder="Ciudad"
-          value={formData.ciudad}
-          onChange={handleChange}
-          required
+          className="tecnico-input"
+          value={ciudad}
+          onChange={(e) => setCiudad(e.target.value)}
         />
-        <button type="submit" className="add-button">
-          Agregar Técnico
-        </button>
-      </form>
-
-      <ul className="tecnicos-list">
-        {tecnicos.map((tecnico) => (
-          <li key={tecnico.id_tecnico}>
-            {tecnico.nombre} - {tecnico.ciudad}
-            <button
-              onClick={() => handleDelete(tecnico.id_tecnico)}
-              className="delete-button"
-            >
-              Eliminar
-            </button>
-          </li>
-        ))}
-      </ul>
+        <div className="gestionar-tecnico-password-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Contraseña"
+            className="tecnico-input password-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <img
+            src={showPassword ? hideIcon : showIcon}
+            alt="Toggle Password Visibility"
+            className="gestionar-tecnico-toggle-password-icon"
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        </div>
+        <div className="tecnico-buttons equal-width">
+          <button
+            className="tecnico-button actualizar"
+            onClick={handleActualizarTecnico}
+            disabled={!isEditMode}
+          >
+            Actualizar
+          </button>
+          <button
+            className="tecnico-button eliminar"
+            onClick={handleEliminarTecnico}
+            disabled={!isEditMode}
+          >
+            Eliminar
+          </button>
+          <button
+            className="tecnico-button agregar full-width"
+            onClick={handleAgregarTecnico}
+            disabled={isEditMode}
+          >
+            Añadir
+          </button>
+        </div>
+      </div>
       <button
         className="back-button"
         onClick={() => navigate("/dashboard-admin")}
       >
         Atrás
       </button>
+      {showAlert && (
+        <Alert
+          message={alertMessage}
+          onClose={() => {
+            setShowAlert(false);
+            setAlertMessage("");
+          }}
+        />
+      )}
     </div>
   );
 };
